71cd62403f90e57bb3a216c48305a821f596405d
philo <philopodickens92@gmail.com>
2021-07-17 20:03:24 +0300

    add variables and function to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var weekDays= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+function findAkanName(){ 
+  var day = parseInt(document.getElementById("day").value);
+  var month = parseInt(document.getElementById("month").value);
+  var year = parseInt(document.getElementById("year").value);
+  var male = document.getElementById("male");
+  var female = document.getElementById("female");
+ 
