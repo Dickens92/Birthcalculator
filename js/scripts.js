1a5fbc89804c69fc1c652b52476e34ed912239ab
philo <philopodickens92@gmail.com>
2021-07-17 20:31:20 +0300

    add conditions to the function to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var weekDays= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
 
 function findAkanName(){ 
   var day = parseInt(document.getElementById("day").value);
@@ -8,4 +9,10 @@ function findAkanName(){
   var year = parseInt(document.getElementById("year").value);
   var male = document.getElementById("male");
   var female = document.getElementById("female");
- 
+}
+  if(day >31 || day <=0){
+    alert("Kindly key in a valid day")
+}
+else if(month >12 || month <=0 || (month == 2 && day > 29)){
+   alert("Kindly key in a valid month") 
+}
