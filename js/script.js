964bf8020ec3642de51cbed3a1ee0c96ff4ee11e
philo <philopodickens92@gmail.com>
2021-07-18 10:56:24 +0300

    add edits to index.html and script.js

--- js/script.js
+++ js/script.js
@@ -4,26 +4,28 @@ var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
 
 function findAkanName(){ 
-  var DD = parseInt(document.getElementById("day").value);
-  var MM = parseInt(document.getElementById("month").value);
-  var YY = parseInt(year.substring (2,4));
-  var CC = parseInt(0,2)
-  var Year= parseInt(document.getElementById("year").value);
+  var day = parseInt(document.getElementById("day").value);
+  var month= parseInt(document.getElementById("month").value);
+  var Year = parseInt(year.substring (2,4));
+  var Century = parseInt(year.substring (0,2));
   var male = document.getElementById("male");
   var female = document.getElementById("female");
-  d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
-  console.log(d);
-  return (Math.floor(d));
-}
+  
   if(day >31 || day <=0){
     alert("Kindly key in a valid day")
 }
 else if(month >12 || month <=0 || (month == 2 && day > 29)){
    alert("Kindly key in a valid month") 
 }
-if (male.checked===true) {
+
+var d = ( ( (Century/4) -2*Century-1) + ( (5*year/4) ) + ((26*(month+1)/10) ) + day)%7;
+  console.log(d);
+  return (Math.floor(d));
+if (male.checked==true) {
   alert (daysOfTheWeek [d] + "was the day of the week you were born" + "Matching Akan name is: " + maleNames[d]);
 }
-else if(female.checked===true) {
+else if(female.checked==true) {
   alert(daysOfTheWeek [d] + "Was the day of the week you were born" + "Matching Akan name is: " + femaleNames[d])
+}
+
 }
