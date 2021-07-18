f09a7554d0dcb5bc411d0655fe8255a3bb75f32d
philo <philopodickens92@gmail.com>
2021-07-18 12:56:36 +0300

    add edits on script.js and edits on index.html

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-var weekDays= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var femaleTitle = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var maleTitles= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
 
-function findAkanName(){ 
+function findAkanName() {
   var day = parseInt(document.getElementById("day").value);
-  var month= parseInt(document.getElementById("month").value);
-  var Year = parseInt(year.substring (2,4));
-  var Century = parseInt(year.substring (0,2));
+  var month = parseInt(document.getElementById("month").value);
+  var Year = parseInt(year.substring(2, 4));
+  var Century = parseInt(year.substring(0, 2));
   var male = document.getElementById("male");
   var female = document.getElementById("female");
-  
-  if(day >31 || day <=0){
-    alert("Kindly key in a valid day")
-}
-else if(month >12 || month <=0 || (month == 2 && day > 29)){
-   alert("Kindly key in a valid month") 
-}
 
-var d = ( ( (Century/4) -2*Century-1) + ( (5*year/4) ) + ((26*(month+1)/10) ) + day)%7;
-  console.log(d);
-  return (Math.floor(d));
-if (male.checked==true) {
-  alert (daysOfTheWeek [d] + "was the day of the week you were born" + "Matching Akan name is: " + maleNames[d]);
-}
-else if(female.checked==true) {
-  alert(daysOfTheWeek [d] + "Was the day of the week you were born" + "Matching Akan name is: " + femaleNames[d])
-}
+  if (day > 31 || day <= 0) {
+    alert("Kindly key in a valid day")
+  }
+  else if (month > 12 || month <= 0 || (month == 2 && day > 29)) {
+    alert("Kindly key in a valid month")
+  }
+
+  var d = (((Century/4) -2*Century-1) + ((5*year/4)) + ((26*(month+1)/10)) + day)%7;
+  
+
+  if(maleTitles.checked == true){
+    alert("was the day of the week you were born " + weekDays[d] + "Matching Akan name is: " + maleTitles[d]);
+  }
+  else if (femaleTitle.checked == true) {
+    alert("Was the day of the week you were born " + weekDays[d] + "Matching Akan name is: " + femaleTitle[d])
+  }
 
 }
