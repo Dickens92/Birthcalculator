5037a3df230b8aaec903ecef7ea26c96074c0e0e
philo <philopodickens92@gmail.com>
2021-07-18 09:23:56 +0300

    add DD, MM, YY, CC and d formilar to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,19 @@ var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
 
 function findAkanName(){ 
-  var day = parseInt(document.getElementById("day").value);
-  var month = parseInt(document.getElementById("month").value);
-  var year = parseInt(document.getElementById("year").value);
+  var DD = parseInt(document.getElementById("day").value);
+  var MM = parseInt(document.getElementById("month").value);
+  var YY = parseInt(year.substring (2,4));
+  var CC = parseInt(0,2)
+  var Year= parseInt(document.getElementById("year").value);
   var male = document.getElementById("male");
   var female = document.getElementById("female");
+  d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
+  console.log(d);
+  return (Math.floor(d));
 }
+}
+var
   if(day >31 || day <=0){
     alert("Kindly key in a valid day")
 }
