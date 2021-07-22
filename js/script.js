25b25728d962438760e8f1b3bfd59edcc6c023ea
philo <philopodickens92@gmail.com>
2021-07-22 07:54:42 +0300

    add edits on scripts.js and index.html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var femaleTitle = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var maleTitles= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var maleTitles = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleTitles = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
 
 
 function findAkanName() {
@@ -9,27 +10,25 @@ function findAkanName() {
   var month = parseInt(document.getElementById("month").value);
   var male = document.getElementById("male");
   var female = document.getElementById("female");
-  var button =document.getElementById("submit");
-  //submit.addEventListener("click", onSubmit)
-  //var d = (((Century/4) -2*Century-1) + ((5*YY/4)) + ((26*(month+1)/10)) + day)%7;
-
-  if(day <=0 || day >31){
+  
+  
+  if (day <= 0 || day > 31) {
     alert("Valid day Required")
-}
-else if(month <=0 || month >12 || (month == 2 && day > 29)){
-   alert("Valid month required") 
-}
+  }
+  else if (month <= 0 || month > 12 || (month == 2 && day > 29)) {
+    alert("Valid month required")
+  }
 
   var A = Math.floor((14 - month) / 12)
   var Y = year - A
   var M = month + 12 * A - 2
-  var D = (day + Y + Math.floor(Y / 4) - Math.floor(Y / 100) + Math.floor(year / 400) + Math.floor((31 * M )/ 12)) % 7;
-  
-  if (male.checked == true){
-      alert("The day of the week you were born is " + weekDays[D] + " and Your Matching Akan name is: " + maleTitles[D]);
+  var D = (day + Y + Math.floor(Y / 4) - Math.floor(Y / 100) + Math.floor(year / 400) + Math.floor((31 * M) / 12)) % 7;
+
+  if (male.checked == true) {
+    alert("The day of the week you were born is " + weekDays[D] + " and Your Matching Akan name is: " + maleTitles[D]);
   }
-  else if (female.checked == true){
-      alert("The day of the week you were born is" + weekDays[D] + " and Your Matching Akan name is: " + femaleTitles[D]);
+  else if (female.checked == true) {
+    alert("The day of the week you were born is " + weekDays[D] + " and Your Matching Akan name is: " + femaleTitles[D]);
   }
 
 }
